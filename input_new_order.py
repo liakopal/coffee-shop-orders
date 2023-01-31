ce591b5cdce19b008b2b4dc2aa8b62984d8670bc
liakopal <liakopoulosalex@gmail.com>
2023-02-01 00:54:01 +0200

    Commit message

--- input_new_order.py
+++ input_new_order.py
@@ -5,27 +5,47 @@
 '''
 
 import uuid
-
-'''
-    The function `input_new_order(db, user)` adds a new order to the database
-    `db` for a specific user `user`
-'''
-    
+from datetime import datetime
 def input_new_order(db, user):
     
-    # Prompt user for customer's name
-    customer_name = input("Please provide customer's name:")
-    
-    # Prompt user for customer's address
-    address = input("Please provide customer's address:")
-    
-    # Prompt user for date
-    date = input("Please provide date:")
-    
+    while True:
+        # Prompt user for customer's name
+        customer_name = input("Please provide customer's name:")
+        
+        if not customer_name.isalpha():
+            print("Error: Customer name is required.")
+            continue
+        else:
+            break
+    while True:
+        # Prompt user for customer's address
+        address = input("Please provide customer's address:")
+        
+        if not address.isalpha():
+            print("Error: Address is required.")
+            continue
+        else:
+            break
+     # Prompt user for date
+    while True:
+        date = input("Please provide date:")
+        try:
+            order_date = datetime.strptime((date), '%d/%m/%Y')
+            break
+        except ValueError:
+                
+            print("Error: Date is required.")
+            continue
+
     # Prompt user for total amount
-    total_amount = input("Please provide total amount:")
-    
-    # Append the order information to the database
+    while True:
+        total_amount = input("Please provide total amount:")
+        try:
+            total_amount = float(total_amount)
+            break
+        except ValueError:
+            print("Error: Total amount is required and must be a number.")
+        # Append the order information to the database
     db[user].append(
         {
             "customer_name": customer_name,
@@ -35,6 +55,7 @@ def input_new_order(db, user):
             "user": user,
             "customer_id": uuid.uuid1()
             # a unique customer ID generated using uuid.uuid1()
+        
         })
     
     # Return the updated database
