04afdfb7cfaf99a0d678095b5f0ee242874cea33
liakopal <liakopoulosalex@gmail.com>
2023-02-01 00:51:25 +0200

    Commit message

--- login.py
+++ login.py
@@ -4,19 +4,19 @@
     If the entered username and password are valid, the function returns the username.
     If the entered username and password are not valid, the function prompts the user to try again.
 '''
-
+import getpass
 def login():
     # Prompt the user to enter their username with a while loop
     while True:
         username = input("Please provide username: ")
         # Prompt the user to enter their password
-        password = input("Please provide password: ")
+        password = getpass.getpass("Please provide password: ")
         # Check if the entered username and password are valid
         if check_user(username, password):
             # Return the username if the entered username and password are valid
             return username
         # Prompt the user to try again if the entered username and password are not valid
-        print("Wrong password, try again...\n")
+        print("Wrong Password, try again...\n")
 
 
 def check_user(username, password):
